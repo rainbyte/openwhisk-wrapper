3eb0d1f799c4e1cbf483c07fed98904868ed4703
Alvaro Fernando Garcia <alvarofernandogarcia@gmail.com>
2016-07-27 15:56:51 -0300

    Mimic dockerSkeleton error handling

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,22 @@
 
 module Main where
 
-import           Data.Aeson (Value)
-import           Network.HTTP.Types.Status (ok200)
+import           Data.Aeson hiding (json)
+import           Data.Aeson.Types (parseMaybe)
+import           Data.Text (Text)
+import           Network.HTTP.Types.Status
 import           Web.Scotty
+
+toServiceArgs :: Value -> Value
+toServiceArgs body =
+  case parseMaybe parser body of
+    Just args -> args
+    Nothing   -> object []
+  where
+    parser = withObject "jsonData" $ \o -> o .: "value"
+
+service :: Value -> Value
+service = id
 
 main :: IO ()
 main = scotty 8080 $ do
@@ -12,4 +25,13 @@ main = scotty 8080 $ do
     status ok200
   post "/run" $ do
     d <- jsonData
-    json (d :: Value)
+    let args = toServiceArgs d
+        result = service args
+    case result of
+      (Object o) -> do
+        json o
+        status ok200
+      _ -> do
+        let errMsg = "result is not an object" :: Text
+        json (object ["error" .= errMsg])
+        status status502
